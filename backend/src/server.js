d92b8cd3fedcd6ad1c7572279d8e75587506706d
larsmhar <lars.martin.hardeland@gmail.com>
2018-10-25 12:01:26 +0200

    Added eslint to build, cors to server #6 #7

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const graphqlHTTP = require( 'express-graphql' );
 const { buildSchema } = require( 'graphql' );
 const sqlite3 = require( 'sqlite3' ).verbose();
 import {promisify} from 'bluebird';
+import cors from 'cors';
 //https://graphql.org/graphql-js/running-an-express-graphql-server/
 //https://medium.com/codingthesmartway-com-blog/creating-a-graphql-server-with-node-js-and-express-f6dddc5320e1
 
@@ -39,6 +40,7 @@ type Movie {
 ` );
 
 const getFilms = function( args ) {
+    console.log( args );
     if ( args.id ) {
         return new Promise( ( resolve, reject ) => {
             db.get( 'SELECT * FROM movie WHERE Id = $id', args.id ).then( function( result ) {
@@ -81,10 +83,12 @@ const root = {
 
 
 const app = express();
-app.use( '/graphql', graphqlHTTP( {
-    'schema': schema,
-    'rootValue': root,
-    'graphiql': true,
-} ) );
+app.use( '/graphql', cors(),
+    graphqlHTTP( {
+        'schema': schema,
+        'rootValue': root,
+        'graphiql': true,
+    } )
+);
 app.listen( 4000 );
 console.log( 'Running a GraphQL API server at localhost:4000/graphql' );

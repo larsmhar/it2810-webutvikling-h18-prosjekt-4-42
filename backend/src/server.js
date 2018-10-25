8657a1b1a72ecf05270d38cb173583a26669f537
KMikalsen <KMikalsen@users.noreply.github.com>
2018-10-25 11:00:12 +0200

    Added error handling and fixed request code
    #5
    Also some linting #6

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-const sqlite3 = require('sqlite3').verbose();
+const express = require( 'express' );
+const graphqlHTTP = require( 'express-graphql' );
+const { buildSchema } = require( 'graphql' );
+const sqlite3 = require( 'sqlite3' ).verbose();
 import {promisify} from 'bluebird';
 //https://graphql.org/graphql-js/running-an-express-graphql-server/
 //https://medium.com/codingthesmartway-com-blog/creating-a-graphql-server-with-node-js-and-express-f6dddc5320e1
 
-const db = new sqlite3.Database('../database.db', (err) => {
-    if (err) {
-        console.log(err.message);
+const db = new sqlite3.Database( '../database.db', ( err ) => {
+    if ( err ) {
+        console.log( err.message );
     }
-    console.log('Connected to database')
-})
+    console.log( 'Connected to database' );
+} );
 
-db.get = promisify(db.get);
-db.all = promisify(db.all);
+db.get = promisify( db.get );
+db.all = promisify( db.all );
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema( `
   type Query {
     films(id:String, year:String): [Movie]
 },
@@ -36,57 +36,54 @@ type Movie {
     imdbRating: String
     Production: String
 }
-`);
+` );
 
-var movies = [
-    {
-        id: "t1",
-        title: "Star Wars",
-        year: 1976
-    },
-    {
-        id:"t2",
-        title:"Batman",
-        year:1980
-    },
-    {
-        id:"t3",
-        title:"A movie",
-        year:1976
-    }
-]
+const getFilms = function( args ) {
+    if ( args.id ) {
+        return new Promise( ( resolve, reject ) => {
+            db.get( 'SELECT * FROM movie WHERE Id = $id', args.id ).then( function( result ) {
+                if ( result ) {
+                    resolve( [result] );
+                } else {
+                    reject( Error( 'ID not found' ) );
+                }
+            } );
+        } );
+    } else if ( args.year ) {
+        return new Promise( ( resolve, reject ) => {
 
-var getFilms = function(args) {
-    if ( !!args.id) {
-        return new Promise((resolve, reject) => {
-            db.get('SELECT * FROM movie WHERE Id = $id' ,args.id).then(function(result) {
-                console.log(result)
-                resolve([result]);
-            })
-        });
-    }else if ( !!args.year ){
-        return new Promise((resolve, reject) => {
-            db.all('SELECT * FROM movie WHERE Year = $year' ,args.year).then(function(result) {
-                console.log(result)
-                resolve(result);
-            })
-        });
+            db.all( 'SELECT * FROM movie WHERE Year = $year', args.year ).then( function( result ) {
+                if ( result ) {
+                    resolve( [result] );
+                } else {
+                    reject( Error( 'Year not found' ) );
+                }
+            } );
+
+        } );
     }
-    return movies;
-}
+    return new Promise( ( resolve, reject ) => {
+        db.all( 'SELECT * FROM movie' ).then( function( result ) {
+            if ( result ) {
+                resolve( [result] );
+            } else {
+                reject( Error( 'Movies not found?' ) );
+            }
+        } );
+    } );
+};
 
 // The root provides a resolver function for each API endpoint
-var root = {
-  films: getFilms
+const root = {
+    'films': getFilms
 };
 
 
-
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+const app = express();
+app.use( '/graphql', graphqlHTTP( {
+    'schema': schema,
+    'rootValue': root,
+    'graphiql': true,
+} ) );
+app.listen( 4000 );
+console.log( 'Running a GraphQL API server at localhost:4000/graphql' );
